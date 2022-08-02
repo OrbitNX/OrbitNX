27eb2cc1c5fa06c5ea91d9fc5eb2bf79da68d424
KayoticCarnige <32397453+kckarnige@users.noreply.github.com>
2022-08-01 23:58:08 -0400

    Boredom

--- preload.js
+++ preload.js
@@ -10,14 +10,19 @@ function installPyReq () {
 window.addEventListener('DOMContentLoaded', () => {
     document.getElementById("payload_1").innerText = payloadConfig.payloadName_1;
     document.getElementById("payload_1").addEventListener("click", function () {
-        injectNXPL(payloadConfig.payloadDir_1, payloadConfig.payload_1);
+        injectNXPL(payloadConfig.payloadDir_1, payloadConfig.payloadFile_1);
     });
 
     document.getElementById("payload_2").innerText = payloadConfig.payloadName_2;
     document.getElementById("payload_2").addEventListener("click", function () {
-        injectNXPL(payloadConfig.payloadDir_2, payloadConfig.payload_2);
+        injectNXPL(payloadConfig.payloadDir_2, payloadConfig.payloadFile_2);
     });
-    
+
+    document.getElementById("payload_3").innerText = payloadConfig.payloadName_3;
+    document.getElementById("payload_3").addEventListener("click", function () {
+        injectNXPL(payloadConfig.payloadDir_3, payloadConfig.payloadFile_3);
+    });
+
     document.getElementById("installPyReq").addEventListener("click", function () {
         installPyReq();
     });
